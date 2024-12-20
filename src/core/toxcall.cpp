289d6055aa6060df1c10487c671bd67cdf7ed6f7
iphydf <iphydf@users.noreply.github.com>
2024-12-20 23:51:37 +0000

    fix: Make camera input work again on macOS.
    It was broken with newer ffmpeg versions. This change aligns the device
    input names with the expectation of newer ffmpeg. Tested against 7.1,
    but it's the same on 6.x and maybe some versions before as well.

--- src/core/toxcall.cpp
+++ src/core/toxcall.cpp
@@ -152,6 +152,10 @@ ToxFriendCall::~ToxFriendCall()
 void ToxFriendCall::onAudioSourceInvalidated()
 {
     auto newSrc = audio.makeSource();
+    if (newSrc == nullptr) {
+        qWarning() << "Failed to create a new audio source";
+        return;
+    }
     connect(newSrc.get(), &IAudioSource::frameAvailable, this,
             [this](const int16_t* pcm, size_t samples, uint8_t chans, uint32_t rate) {
                 av->sendCallAudio(friendId, pcm, samples, chans, rate);
@@ -196,6 +200,11 @@ ToxConferenceCall::ToxConferenceCall(const Conference& conference_, CoreAV& av_,
     : ToxCall(false, av_, audio_)
     , conference{conference_}
 {
+    if (audioSource == nullptr) {
+        qWarning() << "Created conference call without working audio source";
+        return;
+    }
+
     // register audio
     connect(audioSource.get(), &IAudioSource::frameAvailable, this,
             [this](const int16_t* pcm, size_t samples, uint8_t chans, uint32_t rate) {
@@ -219,7 +228,11 @@ ToxConferenceCall::~ToxConferenceCall()
 void ToxConferenceCall::onAudioSourceInvalidated()
 {
     auto newSrc = audio.makeSource();
-    connect(audioSource.get(), &IAudioSource::frameAvailable, this,
+    if (newSrc == nullptr) {
+        qWarning() << "Failed to create a new audio source";
+        return;
+    }
+    connect(newSrc.get(), &IAudioSource::frameAvailable, this,
             [this](const int16_t* pcm, size_t samples, uint8_t chans, uint32_t rate) {
                 if (conference.getPeersCount() <= 1) {
                     return;
